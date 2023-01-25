0cd17879fec038ab16250a811ca9b7ad7e87802a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-25 17:08:22 +0100

    Loop options added and graphic elements added

--- js/main.js
+++ js/main.js
@@ -7,7 +7,25 @@ const ul = document.querySelector("grid")
 console.log("loop-start")
 
 for (let i = 0; i <= 100; i++) {
-    grid.innerHTML += `<li class="box box--${i}">element ${i}</li>`
+
+    // Stampa dei multipli di 3
+
+    if (i % 3 == 0) {
+        grid.innerHTML += `<li class="box box3x">Fizz</li>`
+    }
+
+    // Stampa dei multipli di 5
+
+    else if (i % 5 == 0) {
+        grid.innerHTML += `<li class="box box5x">Buzz</li>`
+    }
+
+    // Stampa dei rimanenti numeri
+
+    else {
+        grid.innerHTML += `<li class="box boxNx">element ${i}</li>`
+    }
+
 }
 
 console.log("loop-end")
