f99b4fdf0127cdc96f18e55370ddc5b21745646a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-25 15:51:31 +0100

    Loop completed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // DOM ELEMENT
 
-const element = document.querySelector("grid")
+const ul = document.querySelector("grid")
 
 // CICLO
 
 console.log("loop-start")
 
 for (let i = 0; i <= 100; i++) {
-    element.innerHTML = <li class="box box--${i}">element ${i}</li>
+    grid.innerHTML += `<li class="box box--${i}">element ${i}</li>`
 }
+
+console.log("loop-end")
