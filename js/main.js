bc38d79ea75664cf436288121c36369e13ddbe77
GabrieleSalati <gabriele.salati5@gmail.com>
2023-01-25 17:30:58 +0100

    Consegna completata!

--- js/main.js
+++ js/main.js
@@ -8,9 +8,15 @@ console.log("loop-start")
 
 for (let i = 1; i <= 100; i++) {
 
+    // Stampa dei multipli di 3 e 5
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        grid.innerHTML += `<li class="box box3x5x">FizzBuzz</li>`
+    }
+
     // Stampa dei multipli di 3
 
-    if (i % 3 == 0) {
+    else if (i % 3 == 0) {
         grid.innerHTML += `<li class="box box3x">Fizz</li>`
     }
 
@@ -18,12 +24,6 @@ for (let i = 1; i <= 100; i++) {
 
     else if (i % 5 == 0) {
         grid.innerHTML += `<li class="box box5x">Buzz</li>`
-    }
-
-    // Stampa dei multipli di 3 e 5
-
-    else if ((i % 3 == 0) && (i % 5 == 0)) {
-        grid.innerHTML += `<li class="box box3x5x">FizzBuzz</li>`
     }
 
     // Stampa dei numeri rimanenti
